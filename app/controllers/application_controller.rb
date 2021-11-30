e2d00aee01e6e05e7dcd02241c7de5737533db7e
Daniel Suarez <suarez.daniel101@gmail.com>
2021-11-30 17:41:07 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,43 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    # @user = User.new(username: params["username"], password: params["password"], balance: params["balance"])
+    # @user.save
+    @user = User.find_by(username: params[:username], password: params[:password])
+    
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    @user = Helpers.is_logged_in?(session)
+    if @user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
-
+  get '/error' do
+    erb :error
+  end
 end
+
+
+# get '/account' do
+#   if Helpers.is_logged_in?(session)
+#     erb :account
+#   else
+#     erb :error
+#   end
+# end
 
